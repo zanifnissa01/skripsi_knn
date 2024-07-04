5332cb1156f9376743351d2c6e4433414c2b6dfe
zanifnissa01 <90230049+zanifnissa01@users.noreply.github.com>
2024-07-05 00:14:40 +0700

    Commit Awal

---
+++ assets/conn/config.php
@@ -0,0 +1,6 @@
+<?php
+$host='localhost';
+$user='root';
+$pass='';
+$db='db_skripsi_knn';
+$conn=mysqli_connect($host,$user,$pass,$db) OR DIE('Tidak terkoneksi ke server!') ?>
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DATA MINING METODE KNN</title>
+    <!-- CDN CSS bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- CDN ICON bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col align-self-center">
+                <form action="index.php" method="POST" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <label for="inputPassword5" class="form-label">Username</label>
+                        <input class="form-control" type="text" placeholder="Username" aria-label="default input example" name="username">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="inputPassword5" class="form-label">Password</label>
+                        <input class="form-control" type="password" placeholder="Password" aria-label="default input example" name="password">
+                    </div>
+
+                    <input type="submit" value="LOGIN" class="btn btn-primary">
+                </form>
+            </div>
+    </div>
+    </div>
+    <!-- CDN CSS bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
